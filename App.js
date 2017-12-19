e5611ed255d396f28bfb006af6143fd7658cb678
irshadxelpmoc <irshad@xelpmoc.in>
2017-12-19 17:46:17 +0530

    new file

--- App.js
+++ App.js
@@ -6,18 +6,38 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import { DrawerNavigator } from "react-navigation";
-import ScreenFirst from "./src/ScreenFirst";
-import ScreenTwo from "./src/ScreenTwo";
+import DrawerExample from "./src/DrawerExample";
 
-const DrawerExample = DrawerNavigator(
-  {
-    ScreenFirst: { screen: ScreenFirst },
-    ScreenTwo: { screen: ScreenTwo }
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <DrawerExample />
+      </View>
+    );
   }
-  // {
-  //   drawerPosition: "right",
-  //   drawerWidth: 100
-  // }
-);
-export default DrawerExample;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
